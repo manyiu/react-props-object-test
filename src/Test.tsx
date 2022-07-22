08ee860afb81c437d58abe3490cd5c857b44977f
Man Yiu <11912398+manyiu@users.noreply.github.com>
2022-07-22 11:12:04 +0800

    first commit

---
+++ src/Test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface TestProps {
+  testObject: {
+    testName: string;
+    testAge: number;
+  };
+}
+
+const Test: React.FC<TestProps> = (props) => {
+  return (
+    <div>
+      <h1>{props.testObject.testName}</h1>
+      <p>{props.testObject.testAge}</p>
+    </div>
+  );
+};
+
+export default React.memo(Test);
